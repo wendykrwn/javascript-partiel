07adbf815a67eea2ba5a6774cebb32663acc762f
Mon Macbook <wendykrwn@gmail.com>
2022-02-23 09:18:06 +0100

    exo 1 : affiche une liste de course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+
 
 function App() {
+  const [items,setItems] = useState(["Salade", "Cornichon","Pain", "Lait", "Chips"])
+
+  const listItems = <div>
+    {
+      items.map((item,index) => <div key={index}>{item}</div>)
+    }
+  </div>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>Liste de courses</h1>
+      {listItems}
     </div>
   );
 }
